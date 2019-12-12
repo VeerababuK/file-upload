934427dabdc46d4f76cf95f5eca58b5cf35bf640
Kanumilli Veerababu <kanumilli.veerababu@gmail.com>
2019-12-12 08:27:42 +0530

    first commit

--- src/main/java/com/veera/uploadfiles/constants/CommonConstants.java
+++ src/main/java/com/veera/uploadfiles/constants/CommonConstants.java
@@ -2,19 +2,19 @@ package com.veera.uploadfiles.constants;
 
 public class CommonConstants {
 
-    public static final String ACCESS_KEY_ID = "AKIAZSLUCF7OLQDX54EP";
+    public static final String ACCESS_KEY_ID = "";
 
-    public static final String ACCESS_SEC_KEY = "qEvbqteZVqd17Be1qUCG34jZ8ofT9ZSgcaIkmFf8";
+    public static final String ACCESS_SEC_KEY = "";
 
-    public static final String BUCKET_NAME = "veera-upload-file-bucket";
+    public static final String BUCKET_NAME = "";
 
     public static final String FOLDER_NAME = "storeFiles";
 
-    public static final String FILE_NAME = "Java coding standards.docx";
+    public static final String FILE_NAME = "";
 
     public static final String FILE_PATH = "C:\\Users\\kanum\\OneDrive\\Desktop\\Java coding standards.docx";
 
-    public static final String BUCKET_FILE_PATH = "storeFiles/Java coding standards.docx";
+    public static final String BUCKET_FILE_PATH = "";
 
     public static final String LOCAL_DOWNLOAD_PATH = "C:\\Users\\kanum\\OneDrive\\Desktop\\Java coding standards.docx";
 
